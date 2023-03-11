e11ca38b1a8b5dd1f64fef25f358ea2ca9b5c2df
Lexakul <52861134+Lexakul@users.noreply.github.com>
2023-03-11 20:52:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 int main() {
 	setlocale(LC_ALL, "Russian");
+	int64_t x;
+	uint16_t n;
 	cout << "Введите целое число которое нужно возвести в степень " << endl;
 	cin >> x; 
 	cout << "Введите положительную целую степень числа " << endl;
