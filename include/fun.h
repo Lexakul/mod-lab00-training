5950f05585aa27ad8efa5ae6cae3bcd6a1685d18
Lexakul <52861134+Lexakul@users.noreply.github.com>
2023-03-11 23:02:59 +0300

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -1,8 +1,7 @@
-#pragma once
+// Copyright 2022 Kulikov
 #ifndef INCLUDE_FUN_H_
 #define INCLUDE_FUN_H_
 #include <cstdint>
-using namespace std;
 //int64_t x;
 //uint16_t n;
 int64_t  power(int64_t x, uint16_t n);
