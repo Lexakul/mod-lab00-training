92a5e11e7e4763b4b45415143fa2a8c1d4074bb6
Lexakul <52861134+Lexakul@users.noreply.github.com>
2023-03-11 21:10:52 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 #include "fun.h"
 #include <iostream>
 int main() {
-	setlocale(LC_ALL, "Russian");
-	int64_t x;
-	uint16_t n;
-	cout << "Введите целое число которое нужно возвести в степень " << endl;
-	cin >> x; 
-	cout << "Введите положительную целую степень числа " << endl;
-	cin >> n;
-	cout << "Ответ: " << power(x, n);
+	int64_t x = 2;
+	uint16_t n = 4;
+	cout <<  power(x, n);
 }
