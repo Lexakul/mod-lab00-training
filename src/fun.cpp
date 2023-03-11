9feeb890f5f94b28178cb3d547ea3a4388816e99
Lexakul <52861134+Lexakul@users.noreply.github.com>
2023-03-11 23:10:09 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,10 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include <iostream>
 #include "fun.h"
-int64_t power(int64_t x, uint16_t n) { 
+int64_t power(int64_t x, uint16_t n) {
     int64_t y = 1;
     for (int i = 0; i < n; i++) {
         y *= x;
     }
-    return y; 
+    return y;
 }
