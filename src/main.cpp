ecdea8e9a8dfa7860db6ff5c5a2c12d814812a55
Lexakul <52861134+Lexakul@users.noreply.github.com>
2023-03-11 22:54:26 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include "fun.h"
 #include <iostream>
+#include "fun.h"
 int main() {
-	int64_t x = 2;
-	uint16_t n = 4;
-	cout <<  power(x, n);
+   int64_t x = 2;
+   uint16_t n = 4;
+   cout <<  power(x, n);
 }
