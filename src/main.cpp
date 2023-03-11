9db4ef6c170e9a7054da1397786021b86480f11f
Lexakul <52861134+Lexakul@users.noreply.github.com>
2023-03-11 20:07:41 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
-
+#include <iostream>
 int main() {
-    return 0;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите целое число которое нужно возвести в степень " << endl;
+	cin >> x; 
+	cout << "Введите положительную целую степень числа " << endl;
+	cin >> n;
+	cout << "Ответ: " << power(x, n);
 }
